75684b16f5aee943078ae4b596a21c107cb1f057
葛上 昌司 <kuzukami+user@altpaper.net>
2020-01-21 11:20:24 +0900

    replaceがすかっていたので修正

--- reverse_proxy-based-arch-examples/kc-mod_auth_openidc-example/reverse_proxy/custom-envsubst.sh
+++ reverse_proxy-based-arch-examples/kc-mod_auth_openidc-example/reverse_proxy/custom-envsubst.sh
@@ -4,6 +4,6 @@ sed "s|https://sso.example.com/auth/realms/demo/.well-known/openid-configuration
 sed "s|dummy.example.com|${OIDCUnmatchedDefaultServerName}|" | \
 sed "s|ServerName.app1.example.com|${OIDCRevProxyFrontServerName}|" | \
 sed "s|http://host.docker.internal:8081/|${OIDCRevProxyBackendServerURL}|" | \
-sed "s|https://app1.example.com/oidc-redirect/|${OIDCRevProxyTokenReceiveURL}|" | \
+sed "s|https://app1.example.com/oidc-redirect/|${OIDRevProxyTokenReceiveURL}|" | \
 sed "s|OIDCClientID.demo-app1|${OIDCClientID}|" | \
 sed "s|OIDCClientSecret.secret|${OIDCClientSecret}|"
--- reverse_proxy-based-arch-examples/kc-mod_auth_openidc-example/reverse_proxy/test/envsubst-walkthru.sh
+++ reverse_proxy-based-arch-examples/kc-mod_auth_openidc-example/reverse_proxy/test/envsubst-walkthru.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# sed "s/OIDCCryptoPassphrase.password/${OIDCCookiePasswd}/" \
-# sed "s/OIDCCookieDomain.example.com/${OIDCCookieDomain}/" \
-# sed "s|https://sso.example.com/auth/realms/demo/.well-known/openid-configuration|${OIDCMetadataURL}|" \
-# sed "s|dummy.example.com|${OIDCUnmatchedDefaultServerName}|" \
-# sed "s|ServerName.app1.example.com|${OIDCRevProxyFrontServerName}|" \
-# sed "s|http://host.docker.internal:8081/|${OIDCRevProxyBackendServerURL}|" | \
-# sed "s|https://app1.example.com/oidc-redirect/|${OIDCRevProxyTokenReceiveURL}|" | \
-# sed "s|OIDCClientID.demo-app1|${OIDCClientID}|" | \
-# sed "s|OIDCClientSecret.secret|${OIDCClientSecret}|"
-
 set -eu
 
 export OIDCCookiePasswd=cpasswd
@@ -25,13 +15,15 @@ export OIDCClientSecret=x12345
 cat ../proxy.conf | \
 bash ../custom-envsubst.sh > .genresult.disposable
 
-fgrep "${OIDCCookiePasswd}" .genresult.disposable
-fgrep "${OIDCCookieDomain}" .genresult.disposable
-fgrep "${OIDCMetadataURL}" .genresult.disposable
-fgrep "${OIDCUnmatchedDefaultServerName}" .genresult.disposable
-fgrep "${OIDCRevProxyFrontServerName}" .genresult.disposable
-fgrep "${OIDCRevProxyBackendServerURL}" .genresult.disposable
-fgrep "${OIDCClientID}" .genresult.disposable
-fgrep "${OIDCClientSecret}" .genresult.disposable
+echo 1; fgrep "${OIDCCookiePasswd}" .genresult.disposable
+echo 2;fgrep "${OIDCCookieDomain}" .genresult.disposable
+echo 3;fgrep "${OIDCMetadataURL}" .genresult.disposable
+echo 4;fgrep "${OIDCUnmatchedDefaultServerName}" .genresult.disposable
+echo 5;fgrep "${OIDCRevProxyFrontServerName}" .genresult.disposable
+echo 6;fgrep "${OIDCRevProxyBackendServerURL}" .genresult.disposable
+echo 7;fgrep "${OIDRevProxyTokenReceiveURL}" .genresult.disposable
+echo 8;fgrep "${OIDCClientID}" .genresult.disposable
+echo 9;fgrep "${OIDCClientSecret}" .genresult.disposable
+echo '9/9 success'
 
 rm -f .genresult.disposable
